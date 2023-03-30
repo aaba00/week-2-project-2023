c411a055b46de560305205e2c6f6905dbd04261d
aaba00 <ab9761@nyu.edu>
2023-03-30 19:32:27 -0400

    importing files from jsfiddle

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//script adapted from: https://www.tutorialrepublic.com/faq/how-to-change-the-image-source-using-jquery.php and https://www.peachpit.com/articles/article.aspx?p=2239154&seqNum=10
+
+var myPix = new Array("https://i.ibb.co/wKmy5Y2/row-1-column-1.jpg", "https://i.ibb.co/PcKrdb5/row-1-column-2.jpg", "https://i.ibb.co/qW9f6xB/row-1-column-3.jpg", "https://i.ibb.co/vXmsdrX/row-2-column-1.jpg", "https://i.ibb.co/WfKPf2s/row-2-column-2.jpg", "https://i.ibb.co/cNp48hd/row-2-column-3.jpg", "https://i.ibb.co/9VpSNH8/row-3-column-1.jpg", "https://i.ibb.co/6DmNMF9/row-3-column-2.jpg", "https://i.ibb.co/23ynDh0/row-3-column-3.jpg");
+var randomNum = Math.floor(Math.random() * myPix.length);
+$(document).ready(function() {
+  $("img").click(function changeImg() {
+    $(this).attr("src", myPix[randomNum]);
+    randomNum = Math.floor(Math.random() * myPix.length);
+  });
+});
