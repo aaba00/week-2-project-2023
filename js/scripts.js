92739ffabe9f98fd326439f2fd08cfa68c04cef9
aaba00 <ab9761@nyu.edu>
2023-04-06 07:19:04 -0400

    clean up comments and add descriptors to code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-//script adapted from: https://www.tutorialrepublic.com/faq/how-to-change-the-image-source-using-jquery.php and https://www.peachpit.com/articles/article.aspx?p=2239154&seqNum=10
+// script purpose: establish an array of images to select from. generate a random number variable that changes with each click. use the random number to select the new image displayed within the grid of images upon click.
+// script adapted from: https://www.tutorialrepublic.com/faq/how-to-change-the-image-source-using-jquery.php and https://www.peachpit.com/articles/article.aspx?p=2239154&seqNum=10
 
 var myPix = new Array("https://i.ibb.co/wKmy5Y2/row-1-column-1.jpg", "https://i.ibb.co/PcKrdb5/row-1-column-2.jpg", "https://i.ibb.co/qW9f6xB/row-1-column-3.jpg", "https://i.ibb.co/vXmsdrX/row-2-column-1.jpg", "https://i.ibb.co/WfKPf2s/row-2-column-2.jpg", "https://i.ibb.co/cNp48hd/row-2-column-3.jpg", "https://i.ibb.co/9VpSNH8/row-3-column-1.jpg", "https://i.ibb.co/6DmNMF9/row-3-column-2.jpg", "https://i.ibb.co/23ynDh0/row-3-column-3.jpg");
 var randomNum = Math.floor(Math.random() * myPix.length);
@@ -8,5 +9,3 @@ $(document).ready(function() {
     randomNum = Math.floor(Math.random() * myPix.length);
   });
 });
-
-// how to make a comment in any language: command + slash
