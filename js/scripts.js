193cec0033a7bfdbf0d633fb31357e185d6572e3
aaba00 <ab9761@nyu.edu>
2023-04-06 07:39:58 -0400

    added IDs to images

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 
 var myPix = new Array("https://i.ibb.co/wKmy5Y2/row-1-column-1.jpg", "https://i.ibb.co/PcKrdb5/row-1-column-2.jpg", "https://i.ibb.co/qW9f6xB/row-1-column-3.jpg", "https://i.ibb.co/vXmsdrX/row-2-column-1.jpg", "https://i.ibb.co/WfKPf2s/row-2-column-2.jpg", "https://i.ibb.co/cNp48hd/row-2-column-3.jpg", "https://i.ibb.co/9VpSNH8/row-3-column-1.jpg", "https://i.ibb.co/6DmNMF9/row-3-column-2.jpg", "https://i.ibb.co/23ynDh0/row-3-column-3.jpg");
 var randomNum = Math.floor(Math.random() * myPix.length);
+
 $(document).ready(function() {
   $("img").click(function changeImg() {
     $(this).attr("src", myPix[randomNum]);
